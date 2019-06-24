5ece15d7a85c501d5b95616da007a15f66f91de6
Kishan Manani <kishan_manani@hotmail.com>
2019-06-24 21:55:02 +0100

    Have human players play against AI

--- rps_app/app/views.py
+++ rps_app/app/views.py
@@ -1,7 +1,14 @@
 from flask import render_template, request
+from rps.game import Game, Move
+from rps.player import AIPlayer, ForcedPlayer
 
 from rps_app.app import app
 from rps_app.app.forms import GameForm
+
+MOVES = {'ROCK': Move.ROCK, 'SCISSORS': Move.SCISSORS, 'PAPER': Move.PAPER}
+
+human_player = ForcedPlayer()
+game = Game(player1=human_player, player2=AIPlayer())
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -10,10 +17,12 @@ def home():
 
     if form.is_submitted():
         move = form.move.data
+        human_player.next_move = MOVES[move.upper()]
+        game.play_round()
         result = dict(
-            move=move,
-            opponent_move='paper',
-            is_winner=True,
+            move=game.history[-1][0],
+            opponent_move=game.history[-1][1],
+            is_winner=game.winners[-1],
         )
         return render_template('home.html', form=form, result=result)
 
