11d52eab13baf9f36b52d0cae5faf381f622a578
Davide Sarra <davide.sarra@gmail.com>
2019-06-24 20:45:43 +0100

    Add home page view and game form

--- rps_app/app/views.py
+++ rps_app/app/views.py
@@ -1,6 +1,20 @@
+from flask import render_template, request
+
 from rps_app.app import app
+from rps_app.app.forms import GameForm
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def home():
-    return 'Welcome to rock, paper, scissors!'
+    form = GameForm(request.form)
+
+    if form.is_submitted():
+        move = form.move.data
+        result = dict(
+            move=move,
+            opponent_move='paper',
+            is_winner=True,
+        )
+        return render_template('home.html', form=form, result=result)
+
+    return render_template('home.html', form=form)
